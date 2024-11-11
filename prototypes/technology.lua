db83506591e2a1fcc0027c9cd7460ecd9e6328fd
zeb-hicks <nimphious@gmail.com>
2024-11-12 05:19:47 +1100

    Remove iron carbide item, technology, and recipes.

--- prototypes/technology.lua
+++ prototypes/technology.lua
@@ -1,33 +1,6 @@
 data.extend({
   {
     type = "technology",
-    name = "carbon-steel",
-    icons = {
-      {
-        icon = "__base__/graphics/technology/steel-processing.png",
-        icon_size = 256,
-      },
-      {
-        icon = "__space-age__/graphics/icons/carbon.png",
-        icon_size = 64,
-        scale = 1.0,
-        shift = { 24, 24 },
-      },
-    },
-    icon_size = 128,
-    effects = {
-      -- { type = "unlock-recipe", recipe = "iron-carbide-powder" },
-      -- { type = "unlock-recipe", recipe = "carbon-steel" },
-      { type = "unlock-recipe", recipe = "thermal-vent" },
-    },
-    prerequisites = { "planet-discovery-vulcanus" },
-    research_trigger = {
-      type = "mine-entity",
-      entity = "ashland-lichen-tree",
-    }
-  },
-  {
-    type = "technology",
     name = "acid-processing",
     icons = {
       {

40480428f8e80e4775ed8e4e1a8a775f93f50a1f
zeb-hicks <nimphious@gmail.com>
2024-11-12 05:35:56 +1100

    Adding settings and locale tweaks.

---
+++ settings.lua
@@ -0,0 +1,65 @@
+data.extend({
+  {
+    type = "bool-setting",
+    name = "planet-picker-modify-fulgora",
+    setting_type = "startup",
+    default_value = true,
+    order = "a-a"
+  },
+  {
+    type = "bool-setting",
+    name = "planet-picker-modify-vulcanus-trees",
+    setting_type = "startup",
+    default_value = true,
+    order = "a-b"
+  },
+  {
+    type = "bool-setting",
+    name = "planet-picker-modify-vulcanus-generator",
+    setting_type = "startup",
+    default_value = true,
+    order = "a-b"
+  },
+  {
+    type = "bool-setting",
+    name = "planet-picker-modify-vulcanus-mining",
+    setting_type = "startup",
+    default_value = true,
+    order = "a-b"
+  },
+  {
+    type = "bool-setting",
+    name = "planet-picker-nauvis",
+    setting_type = "startup",
+    default_value = true,
+    order = "b-a"
+  },
+  {
+    type = "bool-setting",
+    name = "planet-picker-gleba",
+    setting_type = "startup",
+    default_value = true,
+    order = "b-b"
+  },
+  {
+    type = "bool-setting",
+    name = "planet-picker-fulgora",
+    setting_type = "startup",
+    default_value = true,
+    order = "b-c"
+  },
+  {
+    type = "bool-setting",
+    name = "planet-picker-vulcanus",
+    setting_type = "startup",
+    default_value = true,
+    order = "b-d"
+  },
+  {
+    type = "bool-setting",
+    name = "planet-picker-aquilo",
+    setting_type = "startup",
+    default_value = false,
+    order = "b-e"
+  },
+})

346521b7fc4e3eeeb400ebdc07bb3bd9751ee462
zeb-hicks <nimphious@gmail.com>
2024-11-14 10:52:00 +1100

    Disable loading debug

--- gui/loading.lua
+++ gui/loading.lua
@@ -18,9 +18,9 @@ GUI.spawn_loading_screen = function(player)
   local frame = gui.add{type = "frame", name = "loading_screen", caption = "Loading...", style="frame"}
   local flow = frame.add{type = "flow", name = "loading_flow", direction = "vertical"}
   local progress_bar = flow.add{type = "progressbar", name = "loading_progress", value = 0, style="planet_loading_bar" }
-  for _, planet in pairs(PlanetSelect.planets) do
-    flow.add{type = "label", name = planet.name.."_progress", caption = planet.name..": 0/0"}
-  end
+  -- for _, planet in pairs(PlanetSelect.planets) do
+  --   flow.add{type = "label", name = planet.name.."_progress", caption = planet.name..": 0/0"}
+  -- end
 end
 
 GUI.update_progress = function(player)
@@ -36,14 +36,14 @@ GUI.update_progress = function(player)
   if not progress_bar then return end
   progress_bar.value = value
 
-  for k, v in pairs(PlanetSelect.progress) do
-    local label = player.gui.center.loading_screen.loading_flow[k.."_progress"]
-    if label then
-      local generated = math.floor((v.chunks.generated / v.chunks.total) * 100) .. "%"
-      local charted = math.floor((v.chart.charted / v.chart.total) * 100) .. "%"
-      label.caption = k..": Generated "..generated.." Charted "..charted
-    end
-  end
+  -- for k, v in pairs(PlanetSelect.progress) do
+  --   local label = player.gui.center.loading_screen.loading_flow[k.."_progress"]
+  --   if label then
+  --     local generated = math.floor((v.chunks.generated / v.chunks.total) * 100) .. "%"
+  --     local charted = math.floor((v.chart.charted / v.chart.total) * 100) .. "%"
+  --     label.caption = k..": Generated "..generated.." Charted "..charted
+  --   end
+  -- end
 
   if value == 1 then
     player.gui.center.loading_screen.destroy()

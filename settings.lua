4c143beb6a7d2b40338349eb2b90865c2b64b27e
zeb-hicks <nimphious@gmail.com>
2024-11-14 10:29:45 +1100

    Planet spawn setting is now runtime.

--- settings.lua
+++ settings.lua
@@ -11,7 +11,14 @@ data.extend({
     name = "planet-picker-modify-fulgora-ice",
     setting_type = "startup",
     default_value = true,
-    order = "a-a"
+    order = "a-b"
+  },
+  {
+    type = "bool-setting",
+    name = "planet-picker-modify-fulgora-sulfur",
+    setting_type = "startup",
+    default_value = true,
+    order = "a-c"
   },
   {
     type = "bool-setting",
@@ -37,35 +44,35 @@ data.extend({
   {
     type = "bool-setting",
     name = "planet-picker-nauvis",
-    setting_type = "startup",
+    setting_type = "runtime-global",
     default_value = true,
     order = "c-a"
   },
   {
     type = "bool-setting",
     name = "planet-picker-gleba",
-    setting_type = "startup",
+    setting_type = "runtime-global",
     default_value = true,
     order = "c-b"
   },
   {
     type = "bool-setting",
     name = "planet-picker-fulgora",
-    setting_type = "startup",
+    setting_type = "runtime-global",
     default_value = true,
     order = "c-c"
   },
   {
     type = "bool-setting",
     name = "planet-picker-vulcanus",
-    setting_type = "startup",
+    setting_type = "runtime-global",
     default_value = true,
     order = "c-d"
   },
   {
     type = "bool-setting",
     name = "planet-picker-aquilo",
-    setting_type = "startup",
+    setting_type = "runtime-global",
     default_value = false,
     order = "c-e"
   },
